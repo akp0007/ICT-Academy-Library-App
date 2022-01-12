11fc8b30b80a09a46858444b09a836d4c578c6ed
akp0007 <93529835+akp0007@users.noreply.github.com>
2022-01-12 09:58:44 -0800

    Updated Properly

--- src/models/userdata.js
+++ src/models/userdata.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://enhanceme:8XznLPRErB7jZWv@cluster0.mke2a.mongodb.net/Libraryapp?retryWrites=true&w=majority');
+mongoose.connect('mongodb://udxtockwi6nsfbk2qsva:r4k5tDDWGYWrZpRJqgS8@bxvmggcbkxcwzws-mongodb.services.clever-cloud.com:27017/bxvmggcbkxcwzws');
 //mongoose.connect('mongodb://localhost:27017/library');
 //mongoose.connect('mongodb://username:password@host:port/database');
 
